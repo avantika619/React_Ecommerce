bcae38dccfab3eba0b3faf7b54491a045e7b6e42
manojvsp <gmanojece2021@gmail.com>
2023-12-09 16:59:47 +0530

    second

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/React_Ecommerce/'
+  base:'/React_Ecommerce/',
+  mode:'hash',
+  build: {
+    assetsInlineLimit: 0,
+    chunkSizeWarningLimit: 1500,
+    cssCodeSplit: true,
+    // ...
+  }
 })
 
