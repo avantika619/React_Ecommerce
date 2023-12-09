46762e36f9e66d74c34807f8f46d32491bd302b6
manojvsp <gmanojece2021@gmail.com>
2023-12-09 17:33:45 +0530

    commit yml

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base:'/React_Ecommerce/',
-  mode:'hash',
-  build: {
-    assetsInlineLimit: 0,
-    chunkSizeWarningLimit: 1500,
-    cssCodeSplit: true,
-    // ...
-  }
 })
 
