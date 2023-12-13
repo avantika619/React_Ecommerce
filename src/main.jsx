bf6f4d0d1a41d55dcf7cc42b0e80f70824fbe2ef
manojvsp <gmanojece2021@gmail.com>
2023-12-13 12:39:31 +0530

    hash router

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import ShopContextProvider from './context/ShopContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-    <BrowserRouter>
+    <HashRouter>
+  
     <ShopContextProvider>
     <App />
     </ShopContextProvider>
-    </BrowserRouter>
+  
+    </HashRouter>
 
 )
